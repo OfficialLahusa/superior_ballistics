3a96e1a828819837567a230cd6aac4d98b1d4823
H.M. Trippensee <82240749+Lumex-code@users.noreply.github.com>
2023-10-05 22:41:39 +0200

    Added Item Tooltip to Cannonball and Grapeshot
    Last few commits before release, i can feel it!

--- src/main/java/com/lahusa/superior_ballistics/item/IronCannonballItem.java
+++ src/main/java/com/lahusa/superior_ballistics/item/IronCannonballItem.java
@@ -1,10 +1,21 @@
 package com.lahusa.superior_ballistics.item;
 
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.Formatting;
+import net.minecraft.world.World;
+
+import java.util.List;
 
 public class IronCannonballItem extends Item {
 
     public IronCannonballItem(Settings settings) {
         super(settings);
+    }
+    public void appendTooltip(ItemStack itemStack, World world, List<Text> tooltip, TooltipContext tooltipContext) {
+        tooltip.add(new TranslatableText("item.superior_ballistics.cannonball.tooltip").formatted(Formatting.GRAY));
     }
 }
